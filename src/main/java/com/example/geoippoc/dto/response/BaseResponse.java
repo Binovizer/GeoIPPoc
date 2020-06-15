ddc8eb55d5aa7072a75f3aefc7156c13a4632fac
Mohd Nadeem <mohd.nadeem3464@gmail.com>
2020-06-15 20:43:13 +0530

    Minor enhancement | annotation super builder of lombok

--- src/main/java/com/example/geoippoc/dto/response/BaseResponse.java
+++ src/main/java/com/example/geoippoc/dto/response/BaseResponse.java
@@ -1,15 +1,15 @@
 package com.example.geoippoc.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 /**
  * @author nadeem
  * @version $Id: BaseResponse.java, v 0.1 2020-06-15 7:55 PM
  */
 @Getter
-@AllArgsConstructor
+@SuperBuilder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseResponse {
     private final boolean success;
